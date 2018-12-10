ffab14dcada308f274b08d20279d449cd6a3b497
Robert Desjardins <roberttdesjardins@gmail.com>
2018-12-10 11:05:49 -0800

    completed Question 9

--- ProjectEuler.js
+++ ProjectEuler.js
@@ -184,4 +184,22 @@ function problem8(num) {
   console.log(largestProduct);
 }
 
-problem8(13)
+
+
+//A Pythagorean triplet is a set of three natural numbers, a < b < c, for which,
+//a^2 + b^2 = c^2
+
+//For example, 3^2 + 4^2 = 9 + 16 = 25 = 5^2.
+
+//There exists exactly one Pythagorean triplet for which a + b + c = 1000.
+//Find the product abc.
+function problem9() {
+  for(let a = 1; a < 1000; a++){
+    for(let b = 1; b < 1000; b++){
+      if (Math.pow((1000-(a+b)),2) == (Math.pow(a,2) + Math.pow(b,2))) return [a,b, (1000-(a+b))]
+    }
+  }
+}
+
+console.log(problem9());
+
